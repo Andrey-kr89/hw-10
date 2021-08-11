c76f244449be216a8aaf3f5ae92175cbf04a2df5
Andrey-kr89 <andrey.kri89@gmail.com>
2021-08-11 19:18:15 +0300

    ready task 5

--- src/Streams.java
+++ src/Streams.java
@@ -3,8 +3,6 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.Random;
 import java.util.regex.Pattern;
-import java.util.stream.Collectors;
-import java.util.stream.IntStream;
 import java.util.stream.*;
 
 public class Streams {
